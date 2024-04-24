48b9673aef6add3e350262582e3b041ae5b499f3
huy12asd <164478889+huy12asd@users.noreply.github.com>
2024-04-24 17:05:52 +0700

    Add files via upload

---
+++ 24.04.2024/myAtoi.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    let b = s.toString().trim();
+    
+    if (!/^[0-9+\-]/.test(b)) return 0;
+  
+    let a = parseInt(b);
+  
+    const min = Math.pow(-2, 31);
+    const max = Math.pow(2, 31) - 1;
+  
+    if (isNaN(a)) return 0;
+    if (a < min) return min;
+    if (a > max) return max;
+  
+    return a;
+  };
